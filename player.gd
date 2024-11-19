a5318653ee6229971b2c3fcb190b19e44d0ab88f
nameless-interactive <147716431+nameless-interactive@users.noreply.github.com>
2024-11-19 17:51:30 +0100

    movement angepasst

--- player.gd
+++ player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+@export_group("Character Movement")
+@export var SPEED = 300.0
 
-const SPEED = 300.0
-
+@export var DASH_SPEED = 600.0
+var dashing = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -11,10 +13,26 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * SPEED
+		if dashing:
+			velocity.x = direction.x * DASH_SPEED
+			velocity.y = direction.y * DASH_SPEED
+		else:
+			velocity.x = direction.x * SPEED
+			velocity.y = direction.y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	if $Timer.is_stopped() and dashing:
+		dashing = false
+		print_debug("stopped dashing")
 
 	move_and_slide()
+
+func _input(event):
+	if event.is_action_pressed("dash"):
+		print_debug("dashed")
+		dashing = true
+		$Timer.start()
