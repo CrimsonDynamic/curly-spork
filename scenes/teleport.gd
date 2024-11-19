a01a6e4d7a710b730daa439c12458f80fe2dfa32
River <leonbarratt@protonmail.com>
2024-11-19 21:33:34 +0100

    Teleport Function

---
+++ scenes/teleport.gd
@@ -0,0 +1,17 @@
+class_name teleport
+extends Area2D
+@export var teleport_pos: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
