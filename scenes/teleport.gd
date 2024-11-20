653b32b1d95c530478676d3b5255dd57e36a5964
River <leonbarratt@protonmail.com>
2024-11-20 09:33:31 +0100

    DD

--- scenes/teleport.gd
+++ scenes/teleport.gd
@@ -1,6 +1,8 @@
 extends Area2D
 @onready var arraydoors = [$"../Area2D/Room1Door1/R1D1".global_position,$"../Area2D/Room1Door2/R1D2$Room1Door1/R1D1".global_position,$"../Room1/Room1Door3/R1D3".global_position]
 
+func _ready() -> void:
+	pass
 func _on_body_entered(body: Node2D) -> void:
 	print_debug("collided")
 	if body.is_in_group("Player"):
